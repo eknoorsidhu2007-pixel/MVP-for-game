86b6444586d6263dc07d825f3d9edf5e02c058f0
Eknoor Sidhu <eknoorsidhu2007@gmail.com>
2026-09-06 01:00:54 -0400

    Add RepairPoint:
    trigger zone for engineer repair interaction

--- Assets/_Scripts/Truck/RepairPoint.cs
+++ Assets/_Scripts/Truck/RepairPoint.cs
@@ -1,6 +1,30 @@
 using UnityEngine;
 
-public class RepairPoint
+public class RepairPoint : MonoBehaviour
 {
-    
+    private BreakdownSystem breakdown;
+
+    private void Awake()
+    {
+        breakdown = GetComponentInParent<BreakdownSystem>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        var player = other.GetComponent<PlayerRole>();
+        if (player == null || !player.isLocalPlayer) return;
+
+        if (Input.GetKey(KeyCode.E) && breakdown != null)
+        {
+            breakdown.CmdStartRepair(player.netId);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var player = other.GetComponent<PlayerRole>();
+        if (player == null) return;
+
+        breakdown?.CmdStopRepair(player.netId);
+    }
 }
