15700162e9954d2d1d80faf9e4f1b7d6cf920565
Eknoor Sidhu <eknoorsidhu2007@gmail.com>
2026-09-06 00:17:54 -0400

    Add Waypoint:
    route node with wrong-turn and destination flags

--- Assets/_Scripts/Navigation/Waypoint.cs
+++ Assets/_Scripts/Navigation/Waypoint.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
 
-public class Waypoint
+public class Waypoint : MonoBehaviour
 {
-    
+    public bool isDestination = false;
+    public bool isWrongTurn = false; // Leads to dead end
+    public Waypoint nextWaypoint;
+    public Waypoint alternateRoute; // For wrong turns that loop back
+    public string landmarkName = "";
+
+    [Header("Visuals")]
+    public Color gizmoColor = Color.yellow;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawSphere(transform.position, 2f);
+        
+        if (nextWaypoint != null)
+        {
+            Gizmos.color = isWrongTurn ? Color.red : Color.green;
+            Gizmos.DrawLine(transform.position, nextWaypoint.transform.position);
+        }
+    }
 }
