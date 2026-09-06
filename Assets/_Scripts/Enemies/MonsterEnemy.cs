36dc2dee5b971b382272e2d6b538e0945260d74a
Eknoor Sidhu <eknoorsidhu2007@gmail.com>
2026-09-06 00:59:20 -0400

    Add MonsterEnemy:
    melee monster AI

--- Assets/_Scripts/Enemies/MonsterEnemy.cs
+++ Assets/_Scripts/Enemies/MonsterEnemy.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
 
-public class MonsterEnemy
+public class MonsterEnemy : EnemyAI
 {
-    
+    [Header("Monster")]
+    public float leapRange = 8f;
+    public float leapForce = 10f;
+
+    [Server]
+    protected override void Attack()
+    {
+        // Melee attack on truck
+        base.Attack();
+        
+        // Visual leap effect could go here
+    }
 }
