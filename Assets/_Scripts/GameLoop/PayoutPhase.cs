0dbb31d4a85b1b88dc9e7c1a21be5c5d0296fe55
Eknoor Sidhu <eknoorsidhu2007@gmail.com>
2026-09-06 01:14:41 -0400

    Add PayoutPhase:
    end-of-run results screen

--- Assets/_Scripts/GameLoop/PayoutPhase.cs
+++ Assets/_Scripts/GameLoop/PayoutPhase.cs
@@ -1,6 +1,42 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PayoutPhase
+public class PayoutPhase : MonoBehaviour
 {
-    
+    public static PayoutPhase Instance;
+
+    [Header("UI")]
+    public GameObject payoutPanel;
+    public Text resultText;
+    public Text payoutText;
+    public Text cargoText;
+    public Text penaltyText;
+    public Button returnButton;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        returnButton.onClick.AddListener(ReturnToPrepare);
+        payoutPanel.SetActive(false);
+    }
+
+    public void Show(float payout, float cargo, float penalty, bool won)
+    {
+        payoutPanel.SetActive(true);
+        resultText.text = won ? "QUOTA MET" : "QUOTA FAILED";
+        resultText.color = won ? Color.green : Color.red;
+        payoutText.text = $"Payout: ${payout:F0}";
+        cargoText.text = $"Cargo Bonus: ${cargo:F0}";
+        penaltyText.text = $"Penalties: ${penalty:F0}";
+    }
+
+    private void ReturnToPrepare()
+    {
+        // Reload scene or reset state
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+    }
 }
